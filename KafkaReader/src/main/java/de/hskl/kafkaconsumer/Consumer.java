0124eb6ca0b009d8036a9761518a4d4f67b78f8c
alex9849 <alexander@liggesmeyer.de>
2020-06-01 15:33:41 +0200

    finalized KafkaReader

--- KafkaReader/src/main/java/de/hskl/kafkaconsumer/Consumer.java
+++ KafkaReader/src/main/java/de/hskl/kafkaconsumer/Consumer.java
@@ -6,10 +6,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
@@ -18,23 +15,29 @@ public class Consumer {
     public static void main(String[] args) throws IOException {
         Properties properties = new Properties();
         properties.load(Consumer.class.getClassLoader().getResourceAsStream("consumer.properties"));
-        if(args.length != 0) {
-            File file = new File(args[0]);
-            if(file.exists()) {
-                InputStream stream = new FileInputStream(file);
-                properties.load(stream);
-            } else {
-                System.out.println("Properties file doesn't exist!");
-            }
-        }
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getProperty("kafka.host") + ":" + properties.getProperty("kafka.port"));
+        String kafkaHost = properties.getProperty("kafka.host");
+        String kafkaPort = properties.getProperty("kafka.port");
+        String kafkaTopic = properties.getProperty("kafka.topic");
+
+        if(System.getProperty("kafka.host") != null)
+            kafkaHost = System.getProperty("kafka.host");
+        if(System.getProperty("kafka.port") != null)
+            kafkaPort = System.getProperty("kafka.port");
+        if(System.getProperty("kafka.topic") != null)
+            kafkaTopic = System.getProperty("kafka.topic");
+
+        System.out.println(System.getProperty("kafka.host"));
+        System.out.println(System.getProperty("kafka.port"));
+        System.out.println(System.getProperty("kafka.topic"));
+
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaHost + ":" + kafkaPort);
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.put(ConsumerConfig.GROUP_ID_CONFIG, "java-consumer");
 
 
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
-        consumer.subscribe(Arrays.asList(properties.getProperty("kafka.topic")));
+        consumer.subscribe(Arrays.asList(kafkaTopic));
         try {
             while (true) {
                 ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(2000));
