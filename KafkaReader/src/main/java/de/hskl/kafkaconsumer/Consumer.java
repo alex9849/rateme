72a53fbbdffc36a2436f9a8d3189348e1acab0e1
alex9849 <alexander@liggesmeyer.de>
2020-06-01 14:53:38 +0200

    stuff

--- KafkaReader/src/main/java/de/hskl/kafkaconsumer/Consumer.java
+++ KafkaReader/src/main/java/de/hskl/kafkaconsumer/Consumer.java
@@ -6,25 +6,38 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
 
 public class Consumer {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws IOException {
         Properties properties = new Properties();
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.load(Consumer.class.getClassLoader().getResourceAsStream("consumer.properties"));
+        if(args.length != 0) {
+            File file = new File(args[0]);
+            if(file.exists()) {
+                InputStream stream = new FileInputStream(file);
+                properties.load(stream);
+            } else {
+                System.out.println("Properties file doesn't exist!");
+            }
+        }
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getProperty("kafka.host") + ":" + properties.getProperty("kafka.port"));
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.put(ConsumerConfig.GROUP_ID_CONFIG, "java-consumer");
 
 
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
-        consumer.subscribe(Arrays.asList("rating"));
+        consumer.subscribe(Arrays.asList(properties.getProperty("kafka.topic")));
         try {
             while (true) {
                 ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(2000));
-                System.out.println("Records read: " + records.count());
                 for (ConsumerRecord<String, String> record : records) {
                     System.out.println("offset: " + record.offset() + ", key: " + record.key() + ", value: " + record.value());
                 }
